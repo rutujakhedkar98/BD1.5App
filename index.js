19fd1b5477617b19723b91564ca3fafdbd651549
rutujakhedkar98 <rutuja.khedkar@ecerasystem.com>
2025-02-04 14:00:50 +0530

    -

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+
 
 app.use(express.static('static'));
 
